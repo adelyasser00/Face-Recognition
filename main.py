427c4d48a642b123bad749178f90e141e315e9fe
hayamhiba <es-hayam.ibrahim2023@alexu.edu.eg>
2023-10-26 15:45:16 +0300

    implement pca, use knn classifier

--- main.py
+++ main.py
@@ -1,29 +1,74 @@
+#TODO: shuffle training data a must, testing data maybe?
+#TODO: loop on all values of alpha and report accuracy for every value
+#TODO: loop on all possible values of neighbours  and Plot (or tabulate) the performance measure (accuracy) against the K value
 import numpy as np
 import os
 from PIL import Image
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
 
 # lists for storing the data matrix D and label vector y
 D = []
 y = []
 
-# 2)
-# loop over all the directory archive
-for subject in range(1, 40):
+# 2) Generate the Data Matrix and the Label vector
+for subject in range(1, 41):
     # every subject has 10 images, get 10 images per subject
     imageCount = 0
 
     for image in os.listdir(f'archive/s{subject}'):
-        if imageCount == 10:
-            break
-        imageCount += 1
         temp = Image.open(f'archive/s{subject}/{image}')
         vector = np.array(temp).flatten()
 
+        y.append ( subject )
         D.append(vector)
-        y.append(subject)
 
 # convert the dataMatrix and labels to numpy arrays
 D = np.array(D)
 y = np.array(y)
 
-# 3) Split the Dataset into Training and Test sets
+
+# 3) Split the data-set into Training and Test sets
+training_data = D[::2]
+testing_data = D[1::2]
+
+training_labels = y[::2]
+testing_labels = y[1::2]
+
+# 4) Classification using PCA
+# Calculate Projection Matrix U
+training_mean = np.mean(training_data, axis=0)
+training_std = np.std(training_data, axis=0)
+training_standarized = (training_data - training_mean)/training_std
+
+covariance_matrix = np.cov(training_standarized.T)
+eigenvalues, eigenvectors = np.linalg.eig(covariance_matrix)
+
+#index to sort the eigen vlaues and eigen vectors in decreasing order of eigen values
+idx = np.argsort(eigenvalues)[::-1]
+sorted_eigenvalues = eigenvectors[idx]
+sorted_eigenvectors = eigenvectors[:,idx]
+
+#sum to get the variance fraction to choose how many dimensiosn aka how many eigen vectors
+cumulative_sum = np.cumsum(sorted_eigenvalues)
+
+# alpha=[0.8,0.85,0.9,0.95]    loop on the array and mark accuracy
+alpha = 0.95  # for example
+num_eigenvectors = np.where(cumulative_sum >= alpha)[0][0] + 1
+#final eigen vectors chosen for projection
+U = sorted_eigenvectors[:, :num_eigenvectors]
+
+#project all the data on the eigen vectors
+D_train_pca = np.dot(training_data, U)
+D_test_pca = np.dot(testing_data, U)
+
+
+#training: fitting the points on the graph so the classifier can classify any new testing point
+knn = KNeighborsClassifier(n_neighbors=1)
+knn.fit(D_train_pca, training_labels)
+
+#testing
+predicted_labels = knn.predict(D_test_pca)
+
+#accuracy
+print(f'Accuracy: {accuracy_score(testing_labels, predicted_labels)}')
